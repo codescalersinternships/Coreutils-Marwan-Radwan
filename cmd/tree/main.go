b757bd9046892eea01018ff07a6c983018024e25
MarwanRadwan7 <marwan.swe@outlook.com>
2024-09-11 12:39:53 +0300

    fix: fix error propagation in tree command

--- cmd/tree/main.go
+++ cmd/tree/main.go
@@ -5,32 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-func tree(path string, curDepth int, maxDepth int, numDirs *int, numFiles *int) {
+func tree(path string, curDepth int, maxDepth int, numDirs *int, numFiles *int) error {
 	if curDepth > maxDepth {
-		return
+		return nil
 	}
 
-	file, err := os.ReadDir(path)
+	files, err := os.ReadDir(path)
 	if err != nil {
-		log.Fatalf("Error happened: %v", err)
+		return fmt.Errorf("error happened: %v", err)
 	}
 
-	for _, f := range file {
+	for _, f := range files {
 		for i := 1; i < curDepth; i++ {
 			fmt.Print("│   ")
 		}
 		if f.IsDir() {
 			*numDirs++
 			fmt.Println("├──", f.Name())
-			tree(path+"/"+f.Name(), curDepth+1, maxDepth, numDirs, numFiles)
+			if err := tree(filepath.Join(path, f.Name()), curDepth+1, maxDepth, numDirs, numFiles); err != nil {
+				return err
+			}
 		} else {
 			*numFiles++
 			fmt.Println("└──", f.Name())
 		}
 	}
 
+	return nil
 }
 
 func main() {
@@ -46,10 +50,16 @@ func main() {
 	flag.Parse()
 
 	path := flag.Arg(0)
+	if path == "" {
+		path = "."
+	}
 
 	numFiles := 0
 	numDirs := 0
-	tree(path, 0, int(depth), &numDirs, &numFiles)
+	err := tree(path, 0, int(depth), &numDirs, &numFiles)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	fmt.Printf("%d directories, %d files\n", numDirs, numFiles)
 }
